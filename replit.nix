7dc7e99cbc1d7cd8062b0528a0bd5aa18f74b4d2
bobo <570105891@qq.com>
2024-03-20 17:22:22 +0800

    🐳 chore(replit): 解决pnpm找不到的问题

--- replit.nix
+++ replit.nix
@@ -1,8 +1,9 @@
 { pkgs }: {
   deps = [
-    pkgs.nodejs-18_x
-    pkgs.nodePackages.typescript-language-server
-    pkgs.yarn
-    pkgs.replitPackages.jest
+    pkgs.nodejs-17_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.yarn
+        pkgs.nodePackages.pnpm
+        pkgs.replitPackages.jest
   ];
 }
